dd0493796451f2af6c56bca9d6f17ebe44b6ac4f
vitalics <vitalicset@yandex.ru>
2016-11-28 03:09:12 +0300

    lol/test

--- creator.js
+++ creator.js
@@ -1,5 +1,11 @@
 let fs = require('fs');
 let fse = require('fs-extra');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 function CreateFolder(path) {
     //let fse = require('fs-extra');
@@ -9,16 +15,46 @@ function CreateFolder(path) {
 }
 
 function CreateJson(path) {
-    let jsonDefault = '{"Hello":"node"}';
+    let jsonDefault = '{"main":"index.js"}';
 
     var exjson = { 'key': '...abc...', 'key2': '...xyz...' };
-    console.log(JSON.stringify(exjson));
-    fs.writeFile('message.json', exjson, (err) => {
+
+    fs.writeFile('message.json', jsonDefault, (err) => {
+        Asker();
         if (err) throw err;
         console.log('It\'s saved!');
     });
 }
-CreateJson(__dirname)
+
+function Asker() {
+    rl.question('What is your favorite food?', (answer) => {
+        console.log(`Oh, so your favorite food is ${answer}`);
+    });
+}
+function Writer() {
+    rl.write('Delete me!');
+    // Simulate ctrl+u to delete the line written previously
+
+}
+
+// function Promter() {
+//     rl.setPrompt('OHAI> ');
+//     rl.prompt();
+
+//     rl.on('line', (line) => {
+//         switch (line.trim()) {
+//             case 'hello':
+//                 console.log('world!');
+//                 break;
+//             default:
+//                 console.log('Say what? I might have heard `' + line.trim() + '');
+//                 break;
+//         }
+//         rl.prompt();
+//     }
+// }
+//Writer();
+ CreateJson(__dirname);
 
 module.exports.CreateFolder = CreateFolder;
 module.exports.CreateJson = CreateJson;
